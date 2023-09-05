ce2b7495f26e9dd4fe82f3b0c9ed33b97422e387
Nadia <nadiahosisy@gmail.com>
2023-09-05 20:00:41 +0300

    add icons from react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import people from "./data";
+import { FaBeer } from "react-icons/fa";
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  return <h2>Reviews Starter</h2>;
+  return (
+    <div>
+      <h2>Reviews Starter</h2>;
+      <FaBeer className="" />
+    </div>
+  );
 };
 export default App;
