8e83ddeb99ad531eb60d12105d7c8930b5d71c30
Nadia <nadiahosisy@gmail.com>
2023-09-05 20:49:08 +0300

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ const App = () => {
     <main>
       <article className="review">
         <div className="img-container">
-          <img src="image" alt={name} className="preson-img" />
-          <span>
-            <FaChevronRight />
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
           </span>
         </div>
-        <h4></h4>
-        <p></p>
-        <p></p>
+        <h4>{name}</h4>
+        <p>{job}</p>
+        <p>{text}</p>
       </article>
     </main>
   );
